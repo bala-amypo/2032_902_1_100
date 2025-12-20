624a059aa0338298e53f7e2a4e498b26a96b019f
bala-amypo <bala@amypo.in>
2025-12-20 05:41:35 +0000

    User commit

--- src/main/java/com/example/demo/Service/Implement/VendorServiceImplement.java
+++ src/main/java/com/example/demo/Service/Implement/VendorServiceImplement.java
@@ -1,4 +1,37 @@
-package com.example.demo.Service.Implement;
-public class VendorServiceimplement{
-    
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ValidationException;
+import com.example.demo.model.Vendor;
+import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.VendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VendorServiceImpl implements VendorService {
+
+    private final VendorRepository vendorRepository;
+
+    public VendorServiceImpl(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @Override
+    public Vendor createVendor(Vendor vendor) {
+        if (vendorRepository.existsByVendorName(vendor.getVendorName())) {
+            throw new ValidationException("Vendor name already exists");
+        }
+        return vendorRepository.save(vendor);
+    }
+
+    @Override
+    public Vendor getVendor(Long id) {
+        return vendorRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Vendor> getAllVendors() {
+        return vendorRepository.findAll();
+    }
 }
