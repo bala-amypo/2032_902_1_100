0912d8659d550c32716cb884c5ef250df63dfb82
bala-amypo <bala@amypo.in>
2025-12-20 05:26:34 +0000

    User commit

--- src/main/java/com/example/demo/Entity/ComplianceScore.java
+++ src/main/java/com/example/demo/Entity/ComplianceScore.java
@@ -1,4 +1,25 @@
-package com.example.demo.Entity;
-public class ComplianceScore{
-    
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class ComplianceScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Vendor vendor;
+
+    private Double scoreValue;
+
+    private String rating;
+
+    private LocalDateTime lastEvaluated;
 }
