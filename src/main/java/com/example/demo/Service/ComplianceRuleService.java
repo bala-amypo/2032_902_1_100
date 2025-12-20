fc01311afb53e40f1855860e798b97b2b5d0231d
bala-amypo <bala@amypo.in>
2025-12-20 05:35:34 +0000

    User commit

--- src/main/java/com/example/demo/Service/ComplianceRuleService.java
+++ src/main/java/com/example/demo/Service/ComplianceRuleService.java
@@ -1,4 +1,14 @@
-package com.example.demo.Service;
-public class ComplianceRuleService{
-    
+package com.example.demo.service;
+
+import com.example.demo.model.ComplianceRule;
+
+import java.util.List;
+
+public interface ComplianceRuleService {
+
+    ComplianceRule createRule(ComplianceRule rule);
+
+    List<ComplianceRule> getAllRules();
+
+    ComplianceRule getRule(Long id);
 }
