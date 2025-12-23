7790e8d979bbf2744bfe5e44e623f83fdfc89376
bala-amypo <bala@amypo.in>
2025-12-23 04:47:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vendors")
+@PostMapping("/api/vendors")
 public class VendorController {
 
     private final VendorService vendorService;
