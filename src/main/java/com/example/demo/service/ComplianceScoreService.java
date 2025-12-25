29e466880abf010ac04c5410be78fb3407a97f37
bala-amypo <bala@amypo.in>
2025-12-25 16:43:55 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplianceScoreService.java
+++ src/main/java/com/example/demo/service/ComplianceScoreService.java
@@ -1,13 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.model.ComplianceScore;
-import java.util.List;
 
 public interface ComplianceScoreService {
-
     ComplianceScore evaluateVendor(Long vendorId);
-
     ComplianceScore getScore(Long vendorId);
-
-    List<ComplianceScore> getAllScores();
 }
--- src/main/java/com/example/demo/service/DocumentTypeService.java
+++ src/main/java/com/example/demo/service/DocumentTypeService.java
@@ -1,13 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.model.DocumentType;
-import java.util.List;
 
 public interface DocumentTypeService {
-
-    DocumentType createDocumentType(DocumentType type);
-
-    List<DocumentType> getAllDocumentTypes();
-
-    DocumentType getDocumentType(Long id);
+    DocumentType createDocumentType(DocumentType documentType);
 }
